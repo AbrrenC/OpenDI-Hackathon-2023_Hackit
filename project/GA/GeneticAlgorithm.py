d6c1c0c48b8c087e4be1b3d3dd1e94e182df5b43
Ambreen Hanif <ambreen.hanif@hdr.mq.edu.au>
2023-12-14 13:27:39 +1100

    added raw files for project

---
+++ project/GA/GeneticAlgorithm.py
@@ -0,0 +1,43 @@
+
+import pygad
+import numpy
+
+
+class geneticAlgiorthm():
+    ''' This class will have the gentic algorithm code for the project'''
+    
+    def initialpopulation():
+        input = [4,-2,3.5,5,-11,-4.7]
+        
+        return input
+    
+    def fitness():  
+        ''' This function will calculate the fitness of each individual'''
+        pass           # return the fitness of each individual                  
+    
+    def selection():    
+        ''' This function will select the best individuals'''
+        pass                    
+        
+    def crossover():
+        ''' This function will perform crossover'''
+        pass        
+    
+    def mutation():
+        ''' This function will perform mutation'''
+        pass                
+        
+    def termination():
+        ''' This function will check the termination condition'''
+        pass
+    
+    def run():          
+        ''' This function will run the genetic algorithm'''
+        pass 
+    
+    
+     
+        
+        
+                        
+# Path: project/GA/GeneticAlgorithm.py
---
+++ project/LLM/LLM.py
@@ -0,0 +1,18 @@
+class LLM:
+    ''' This class will access the large language model for the text generation'''
+    
+    def __init__(self):
+        ''' This function will initialize the large language model'''
+        pass
+    
+    def prompt(self):
+        ''' This function will return the prompt'''
+        pass
+    
+    def prompt_template(self):
+        ''' This function will return the prompt template'''
+        pass
+    
+    def prompt_temperature(self):
+        ''' This function will return the prompt temperature'''
+        pass    
